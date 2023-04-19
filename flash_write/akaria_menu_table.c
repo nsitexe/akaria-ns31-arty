7e5c8c0df8db9ad2075be3d6be8a5f5ac61d227a
Shuhei Orikasa <orikasa@hagiwara.co.jp>
2023-04-19 17:08:34 +0900

    delete SF3 test command

--- flash_write/akaria_menu_table.c
+++ flash_write/akaria_menu_table.c
@@ -2,7 +2,6 @@
 
 
 // Add prototype to add menu table
-extern void akaria_sf3_test(void);
 extern void akaria_flash_write(void);
 
 /*---------------------------------------------------------------*/
@@ -14,7 +13,6 @@ extern void akaria_flash_write(void);
 //  ID     Discription                  Function Pointer
 /*---------------------------------------------------------------*/
 const AkariaMenuTable akaria_menu_table[ AKARIA_MENU_MAX ] = {
-  { "f",  "Pmod SF3(SPI0 Flash) test",  akaria_sf3_test},
   { "w",  "Write bootcode to Pmod SF3", akaria_flash_write},
   { "h",  "Help",                       akaria_menu_help  },
   {0, 0, 0}  // Last element
