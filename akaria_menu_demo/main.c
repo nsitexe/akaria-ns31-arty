9daafb55df106a1fd8d875d42b183a6afbaa98ef
Shuhei Orikasa <orikasa@hagiwara.co.jp>
2023-04-21 13:26:43 +0900

    delete commented out lines

--- akaria_menu_demo/main.c
+++ akaria_menu_demo/main.c
@@ -4,31 +4,11 @@
 #include "akaria_led.h"
 #include "akaria_menu.h"
 
-/***
-void  akaria_1sec_led( void ) {
-  static unsigned int cnt = 0;
-  
-  cnt++;
-
-  if(cnt & 0x1){
-    akaria_led_on(LED7);
-  }
-  else{
-    akaria_led_off(LED7);
-  }
-  return;  
-}
-***/
-
-
 int main(void){
 
   // LED Init
   akaria_led_init();
   
-  // LED7 blinks 1sec interval
-  //akaria_timer_set(500, 0, akaria_1sec_led);
-
   printf("\n");
   printf("#####################################################################\n");
   printf("#                                                                   #\n");
